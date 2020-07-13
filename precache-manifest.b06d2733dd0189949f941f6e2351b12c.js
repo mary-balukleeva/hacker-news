b30e002be97f075b68100aaf24b76e2a68698d0d
Mary Balukleeva <mary.balkleeva10@gmail.com>
2020-07-13 17:56:06 +0300

    Updates

---
+++ precache-manifest.b06d2733dd0189949f941f6e2351b12c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600c4165d411604e93a67b30fb3d1eeb",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "a3be1ea344872337cd42",
+    "url": "/hacker-news/static/css/main.58cb3564.chunk.css"
+  },
+  {
+    "revision": "ffe3efe5cf7dda1a9dfc",
+    "url": "/hacker-news/static/js/2.b49b53ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news/static/js/2.b49b53ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3be1ea344872337cd42",
+    "url": "/hacker-news/static/js/main.6ebc6ff5.chunk.js"
+  },
+  {
+    "revision": "a8c426977c76062afd26",
+    "url": "/hacker-news/static/js/runtime-main.6cbea6e0.js"
+  }
+]);
